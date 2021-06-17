a4297b38b4e533caaed1094062934739b9e7c387
Maciej Bartczak <maciekbartczak17@gmail.com>
2021-06-17 15:58:11 +0200

    Fix client variant

--- client/client.py
+++ client/client.py
@@ -62,7 +62,7 @@ class Client:
         global board
         while True:
             board_fen = self.socket.recv(1024).decode('utf-8')
-            self.board = chess.Board(board_fen)
+            self.board =  chess.variant.find_variant(str(self.variant))(board_fen)
             print(f'\n{self.board}')
             self.screen.fill(self.GREY)
             self.draw_board(self.screen)
@@ -226,39 +226,42 @@ class Client:
                                       self.SQUARE_SIZE, self.SQUARE_SIZE))
                     except Exception:
                         pass
-
-            if self.board.is_variant_loss():
+            outcome = self.board.outcome()
+            if outcome is not None:
                 self.game_end = True
-                if self.board.turn == chess.BLACK:
-                    self.msg = 'Black lost due to special variant'
-                else:
-                    self.msg = 'White lost due to special variant'
-
-            if self.board.is_variant_win():
-                self.game_end = True
-                if self.board.turn == chess.BLACK:
-                    self.msg = 'White lost due to special variant'
-                else:
-                    self.msg = 'Black lost due to special variant'
-
-            if self.board.is_variant_draw():
-                self.game_end = True
-                self.msg = 'Draw due to special variant'
-
-            if self.board.is_checkmate():
-                self.game_end = True
-                if self.board.turn == chess.BLACK:
-                    self.msg = 'Black got checkmated'
-                else:
-                    self.msg = 'White got checkmated'
-
-            if self.board.is_stalemate():
-                self.game_end = True
-                self.msg = 'Stalemate'
-
-            if self.board.is_insufficient_material():
-                self.game_end = True
-                self.msg = 'Insufficient material'
+                self.msg = outcome.result()
+            # if self.board.is_variant_loss():
+            #     self.game_end = True
+            #     if self.board.turn == chess.BLACK:
+            #         self.msg = 'Black lost due to special variant'
+            #     else:
+            #         self.msg = 'White lost due to special variant'
+            #
+            # if self.board.is_variant_win():
+            #     self.game_end = True
+            #     if self.board.turn == chess.BLACK:
+            #         self.msg = 'White lost due to special variant'
+            #     else:
+            #         self.msg = 'Black lost due to special variant'
+            #
+            # if self.board.is_variant_draw():
+            #     self.game_end = True
+            #     self.msg = 'Draw due to special variant'
+            #
+            # if self.board.is_checkmate():
+            #     self.game_end = True
+            #     if self.board.turn == chess.BLACK:
+            #         self.msg = 'Black got checkmated'
+            #     else:
+            #         self.msg = 'White got checkmated'
+            #
+            # if self.board.is_stalemate():
+            #     self.game_end = True
+            #     self.msg = 'Stalemate'
+            #
+            # if self.board.is_insufficient_material():
+            #     self.game_end = True
+            #     self.msg = 'Insufficient material'
 
             if self.board.is_check():
                 if self.board.turn == chess.WHITE:
